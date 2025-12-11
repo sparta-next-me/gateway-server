fd4fb3883175a3164ae6b7ffb416518eb0a853a9
kms <minsu00222@gmail.com>
2025-12-11 13:18:41 +0900

    common swagger code add gateway-server v1

---
+++ src/main/java/org/nextme/gatewayserver/MainController.java
@@ -0,0 +1,12 @@
+package org.nextme.gatewayserver;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.RedirectView;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public RedirectView index() {
+        return new RedirectView("/api-docs.html");
+    };
+}
---
+++ src/main/java/org/nextme/gatewayserver/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package org.nextme.gatewayserver.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi userAPI() {
+        return GroupedOpenApi.builder()
+                .group("user-api")
+                .displayName("USER API")
+                .pathsToMatch("/v1/user/**")
+                .build();
+    }
+}
